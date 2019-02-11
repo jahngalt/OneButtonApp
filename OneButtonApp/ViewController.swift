67a43aa2e4a89fe7a61c05b91258fdeebaa3e559
Oleg Kudimov <ovkudimov@gmail.com>
2019-02-11 15:20:41 +0300

    Initial Commit

---
+++ OneButtonApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  OneButtonApp
+//
+//  Created by Oleg Kudimov on 2/11/19.
+//  Copyright © 2019 Oleg Kudimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showAler(){
+        
+    }
+    
+}
+
