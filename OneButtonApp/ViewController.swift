ff7d93d7da4f7f0b2f3f27c378a774d4ad365238
Oleg Kudimov <ovkudimov@gmail.com>
2019-02-13 13:23:07 +0300

    added some functionality

--- OneButtonApp/ViewController.swift
+++ OneButtonApp/ViewController.swift
@@ -9,20 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    
+    //variables and constants
+    var currentValue: Int = 50 //This variable isn't neccecery anymore, but swift provide us to write in a good style
+    var targetValue = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        
     }
 
     @IBAction func showAler(){
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default,
+        
+        let difference = abs(targetValue - currentValue)
+        
+        
+        let message = "The value of the slider is: \(currentValue)" + "\nThe Target value is: \(targetValue)" + "\nThe difference is: \(difference)"
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: message,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok",
+                                   style: .default,
                                    handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
+    }
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+        
+    }
+   
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        slider.value = Float(currentValue)
+        updateLabels() 
     }
     
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
 }
 
