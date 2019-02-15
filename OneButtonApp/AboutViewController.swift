e21ce2d3bdb536107597a9383427dcbac32e0958
Oleg Kudimov <ovkudimov@gmail.com>
2019-02-15 12:48:49 +0300

    Added WebView to load about screen

--- OneButtonApp/AboutViewController.swift
+++ OneButtonApp/AboutViewController.swift
@@ -7,24 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
-
+    //adding action to "Close" button in AboutViewController
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let url  = Bundle.main.url(forResource: "BullsEye",
+                                      withExtension: "html"){
+            let request = URLRequest(url:url)
+            webView.load(request)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
